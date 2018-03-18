ed4eaa0091a000ae00fe8be31612b885109b00dc
Preetharajendran <37337172+Preetharajendran@users.noreply.github.com>
2018-03-18 14:17:42 +0530

    Create largest number.py

---
+++ Beginner level/largest number.py
@@ -0,0 +1,9 @@
+n1,n2,n3=raw_input().split(' ')
+if(int(n1)>=int(n2) and int(n1)>=int(n3)):
+  print n1
+elif(int(n2)>=int(n1) and int(n2)>=int(n3)):
+  print n2
+elif(int(n3)>=int(n1) and int(n3>=n2)):
+  print n3
+else:
+  print("invalid")
